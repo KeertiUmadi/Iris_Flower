0d9d951231dd290bdf3576fb4fd0ec8a4e7ef06b
Keerti Umadi <89504045+KeertiUmadi@users.noreply.github.com>
2022-03-02 19:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Iris Classifier- API")
+st.title("IRIS CLASSIFIER - API")
 
 sl = st.slider('Sepal Length', 4.3, 7.9, 0.5)
 sw = st.slider('Sepal Width', 2.0, 4.4, 0.5)
@@ -9,10 +9,11 @@ pw = st.slider('Petal Width', 0.1,2.5, 0.5)
 from sklearn.datasets import load_iris
 iris = load_iris()
 
-from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier()
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors = 5)
 model.fit(iris.data,iris.target)
 
 op = model.predict([[sl,sw,pl,pw]])
 op = iris.target_names[op[0]]
 st.title(op)
+    
